00e59909999b731513fecab83d3794b0d1205f2d
ariadnaroiba <ariadna.roiba@gmail.com>
2025-05-21 00:22:27 +0200

    Inico menu i estados
    Menu de inico con instrucciones
    Maquina de estados empezada (elimina los componentes del estado anterior tm)

--- App/Source/Game/Intro.cs
+++ App/Source/Game/Intro.cs
@@ -11,30 +11,37 @@ namespace GameJam
     public class Intro : StaticActor
     {
         private readonly string title = "LIGHTBRINGER";
-        private readonly string description = "Consigue que el pequeño demonio llegue al final del recorrido con vida. \n Debes alumbrarlo con tu luz para que no se haga daño, pero ten cuidado,\n tu liz ira diminuyendo con el tiempo.\n Busca Powerups en el mapa para no apagar tu luz mientras matas polillas que intentaran robartela. \n\nLas polillas mueren al hacerles clic con el ratón.\nLa luz seguira tu cursor automaticamente por el mapa.";
-        private readonly string subtitle = "PRESS ENTER TO START";
+        private readonly string mision = "Consigue que el pequeño demonio llegue al final del recorrido con vida.";
+        private readonly string description = "\n\nDebes alumbrarlo con tu luz para que no se haga daño, pero ten cuidado,\n  tu luz ira diminuyendo con el tiempo.\nBusca Powerups en el mapa para no apagar tu luz mientras matas polillas\n  que intentaran robartela.\n\n- La luz seguira tu cursor automaticamente por el mapa.\n- Para absorber la PowerUp deja presionado encima de la poción.\n- Las polillas mueren al hacerles clic con el ratón.";
+        private readonly string subtitle = "PRESS SPACE TO START";
 
         private Font font, titleFont;
-        private Text titleText, descriptionText, subtitleText;
+        private Text titleText, descriptionText, misionText, subtitleText;
         private Color titleColor = Color.Blue;
+        private Color misionColor = Color.Cyan;
         private Color descriptionColor = Color.White;
-        private Color subtitleColor = Color.Cyan;
+        private Color subtitleColor = Color.Blue;
         private Sprite introImage;
+
+        private Color backgroundColor = new Color(30, 30, 60);
         public Intro()
         {
             titleFont = new Font("Data/Fonts/Scratchy Lemon.ttf");
-            font = new Font("Data/Fonts/osifont-gpl2fe.ttf");
-
-            titleText = new Text(title, titleFont, 80);
+            font = new Font("Data/Fonts/osifont-lgpl3fe.ttf");
+                        
+            titleText = new Text(title, titleFont, 100);
             titleText.FillColor = titleColor;
 
+            misionText = new Text(mision, font, 30);
+            misionText.FillColor = misionColor;
+
             descriptionText = new Text(description, font, 28);
-            descriptionText.FillColor = descriptionColor;
+            descriptionText.FillColor = descriptionColor;                                   
 
             subtitleText = new Text(subtitle, titleFont, 40);
             subtitleText.FillColor = subtitleColor;
 
-            introImage = new Sprite(new Texture("Data/Textures/PowerUp/People01.png"));
+            introImage = new Sprite(new Texture("Data/Textures/Player/Character.png"));
         }
 
         public override void Draw(RenderTarget target, RenderStates states)
@@ -45,29 +52,27 @@ namespace GameJam
             var titleBounds = titleText.GetLocalBounds();
             titleText.Position = new Vector2f((size.X - titleBounds.Width) / 2f, 30);
 
+            var misionBounds = misionText.GetLocalBounds();
+            misionText.Position = new Vector2f((size.X - misionBounds.Width) / 2f, 200);
+
             var descBounds = descriptionText.GetLocalBounds();
             descriptionText.Position = new Vector2f((size.X - descBounds.Width) / 2f, (size.Y - descBounds.Height) / 2f);
 
             var subtitleBounds = subtitleText.GetLocalBounds();
             subtitleText.Position = new Vector2f((size.X - subtitleBounds.Width) / 2f, size.Y - subtitleBounds.Height - 40);
 
+            RectangleShape backgroundRect = new RectangleShape(new Vector2f(size.X, size.Y));
+            backgroundRect.FillColor = backgroundColor;
+            target.Draw(backgroundRect, states);
+
             target.Draw(titleText, states);
+            target.Draw(misionText, states);
             target.Draw(descriptionText, states);
             target.Draw(subtitleText, states);
 
-            var spriteBounds = introImage.GetLocalBounds();
-            introImage.Position = new Vector2f((size.X + spriteBounds.Width) / 2f, 60);
+            var imageBounds = introImage.GetLocalBounds();
+            introImage.Position = new Vector2f((size.X + 450) / 2f, size.Y - imageBounds.Height - 40);
             target.Draw(introImage, states);
-        }
-
-        public override void Update(float dt)
-        {
-            base.Update(dt);
-            /*
-            if (SFML.Window.Keyboard.IsKeyPressed(SFML.Window.Keyboard.Key.Space))
-            {
-                //canviar d'estat
-            }*/
-        }
+        }        
     }
 }
--- App/Source/Game/MyGame.cs
+++ App/Source/Game/MyGame.cs
@@ -6,12 +6,14 @@ namespace GameJam
 {
     public class MyGame : Game
     {
+        private string state = "intro";
+        private string previousState = null;
+
         public Hud hud { private set; get; }
         public Background background { get; private set; }
         public Light light { private set; get; }
         public Enemies enemies { private set; get; }
-
-
+        public Intro intro { private set; get; }
 
         private static MyGame instance;
         public static MyGame Get
@@ -31,10 +33,7 @@ namespace GameJam
         }
         public void Init()
         {
-            background = Engine.Get.Scene.Create<Background>();
-            hud = Engine.Get.Scene.Create<Hud>();
-            light = Engine.Get.Scene.Create<Light>();
-            enemies = Engine.Get.Scene.Create<Enemies>();
+            state = "intro";                               
         }
 
         public void DeInit()
@@ -42,7 +41,83 @@ namespace GameJam
         }
         public void Update(float dt)
         {
+            if (state != previousState)
+            {
+                CleanupState(previousState);
 
+                switch (state)
+                {
+                    case "intro":
+                        intro = Engine.Get.Scene.Create<Intro>();
+                        break;
+                    case "game":
+                        background = Engine.Get.Scene.Create<Background>();
+                        hud = Engine.Get.Scene.Create<Hud>();
+                        light = Engine.Get.Scene.Create<Light>();
+                        enemies = Engine.Get.Scene.Create<Enemies>();
+                        state = "playing"; 
+                        break;
+                    case "playing":
+                        break;
+                    case "gameOver":
+                        break;
+                }
+
+                previousState = state;
+            }
+
+            switch (state)
+            {
+                case "intro":
+                    if (Keyboard.IsKeyPressed(Keyboard.Key.Space))
+                    {
+                        state = "game";
+                    }
+                    break;
+                case "playing":
+                    break;
+                case "gameOver":
+                    break;
+            }
+        }
+
+        private void CleanupState(string oldState)
+        {
+            switch (oldState)
+            {
+                case "intro":
+                    if (intro != null)
+                    {
+                        Engine.Get.Scene.Destroy(intro);
+                        intro = null;
+                    }
+                    break;
+                case "game":
+                case "playing":
+                    if (background != null)
+                    {
+                        Engine.Get.Scene.Destroy(background);
+                        background = null;
+                    }
+                    if (hud != null)
+                    {
+                        Engine.Get.Scene.Destroy(hud);
+                        hud = null;
+                    }
+                    if (light != null)
+                    {
+                        Engine.Get.Scene.Destroy(light);
+                        light = null;
+                    }
+                    if (enemies != null)
+                    {
+                        Engine.Get.Scene.Destroy(enemies);
+                        enemies = null;
+                    }
+                    break;
+                case "gameOver":
+                    break;
+            }
         }
     }
 }
