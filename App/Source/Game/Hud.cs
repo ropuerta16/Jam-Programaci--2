cc3f2cc6cdbd0ceda2940e97e26b2c1f325db825
ropuerta16 <rpuertais@edu.tecnocampus.cat>
2025-05-20 19:12:30 +0200

    Hud

--- App/Source/Game/Hud.cs
+++ App/Source/Game/Hud.cs
@@ -1,7 +1,28 @@
-﻿namespace TcGame
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace TcGame
 {
   public class Hud : Actor
   {
+        Text t;
+        Font f;
+        public float health;
+        public Hud()
+        {
+            f = new Font("Data/Fonts/georgia.ttf");
+            t = new Text("100/100",f);
+
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+
+            health -= dt; 
+
+            t.DisplayedString = string.Format(health + "/100");
+        }
   }
 }
 
--- App/Source/Game/MyGame.cs
+++ App/Source/Game/MyGame.cs
@@ -27,51 +27,15 @@ namespace TcGame
     public void Init()
     {
       background = Engine.Get.Scene.Create<Background>();
-      CreatePersonSpawner();
-      CreateOvniSpawner();
-      CreateTankSpawner();
+
     }
-    private void CreatePersonSpawner()
-    {
-      ActorSpawner<Person> spawner;
-      spawner = Engine.Get.Scene.Create<ActorSpawner<Person>>();
-      spawner.MinPosition = new Vector2f(0.0f, -60.0f);
-      spawner.MaxPosition = new Vector2f(1000.0f, -60.0f);
-      spawner.MinTime = 2.0f;
-      spawner.MinTime = 4.0f;
-      spawner.Reset();
-    }
-    private void CreateOvniSpawner()
-    {
-      ActorSpawner<Ovni> spawner;
-      spawner = Engine.Get.Scene.Create<ActorSpawner<Ovni>>();
-      spawner.MinPosition = new Vector2f(0.0f, +1000.0f);
-      spawner.MaxPosition = new Vector2f(900.0f, 1000.0f);
-      spawner.MinTime = 8.0f;
-      spawner.MinTime = 15.0f;
-      spawner.Reset();
-    }
-    private void CreateTankSpawner()
-    {
-      ActorSpawner<Tank> spawner;
-      spawner = Engine.Get.Scene.Create<ActorSpawner<Tank>>();
-      spawner.MinPosition = new Vector2f(0.0f, -200.0f);
-      spawner.MaxPosition = new Vector2f(1000.0f, 0.0f);
-      spawner.MinTime = 8.0f;
-      spawner.MinTime = 10.0f;
-      spawner.Reset();
-    }
+   
     public void DeInit()
     {
     }
     public void Update(float dt)
     {
       
-    }
-    private void DestroyAll<T>() where T : Actor
-    {
-      var actors = Engine.Get.Scene.GetAll<T>();
-      actors.ForEach(x => x.Destroy());
     }
   }
 }
