37a4102637a092c530941211fd4277acfbb485be
ariadnaroiba <ariadna.roiba@gmail.com>
2025-05-20 21:10:56 +0200

    Fonts afegides

--- App/Source/Game/Intro.cs
+++ App/Source/Game/Intro.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
 
 namespace TcGame
 {
     internal class Intro : StaticActor
     {
+        private Font font,titleFont;
         public Intro() 
         {
-
+            titleFont = new Font("Data/Fonts/Scratchy Lemon");
+            font = new Font("Data/Fonts/osifont-gpl2fe");
         }
     }
 }
